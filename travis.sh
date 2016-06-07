3e35f4ca6b435009b7a2364433021cbf124732ce
sarora <somyarora96@gmail.com>
2016-06-07 12:25:40 +0200

    enhance

--- travis.sh
+++ travis.sh
@@ -3,6 +3,7 @@ echo "TRAVIS_COMMIT_RANGE= ${TRAVIS_COMMIT_RANGE}"
 path=$(git --no-pager diff --name-only ${TRAVIS_COMMIT_RANGE})
 paths=( $path )
 counter=0
+flag=0
 check1=${DH_REPO#*/}
 check2=$IMG_TAG
 
@@ -11,7 +12,7 @@ echo ${paths[counter]}
 
 if [ -z "$path" ]
    then
-	echo "No Modifications to this image"
+	echo "No Modifications required."
 fi
 
 while [[ ${paths[counter]} ]];
@@ -27,6 +28,7 @@ while [[ ${paths[counter]} ]];
 	    then
 
 		 travis_wait 40 docker build -t $DH_REPO:$IMG_TAG $DF_PATH
+     flag=1
 
 		 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]
 
@@ -34,9 +36,18 @@ while [[ ${paths[counter]} ]];
 			docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USER" -p="$DOCKER_PASS"
 			travis_wait 40 docker push $DH_REPO
 
+    else
+      echo "No push command executed"
+
 		fi
-	else
-			echo "No Modifications to this image"
-	fi
+
+  fi
+
+  if [ $flag -eq 0 ]
+     then
+    echo "No Modifications to this image"
+
+  fi
+
 	let counter=counter+1;
 done
